64456ae1dbff28521a06ee9eea3d0c7a0fb05753
bchen4 <amybluebell@163.com>
2016-09-03 11:43:56 -0500

    mv sgcount

--- CRISPR/sgcount_fastq.py
+++ CRISPR/sgcount_fastq.py
@@ -16,11 +16,11 @@ def prepare_argparser():
   description = "Get sgRNA counts from fastq file"
   epilog = "For command line options of each command, type %(prog)% COMMAND -h"
   argparser = ap.ArgumentParser(description=description, epilog = epilog)
-  argparser.add_argument("-i","--input",dest = "infile",type=str,required=True, help="input BAM file")
+  argparser.add_argument("-i","--input",dest = "infile",type=str,required=True, help="design file")
   argparser.add_argument("-o","--output",dest = "outfile",type=str,required=True, help="output")
   argparser.add_argument("-l","--library",dest="libfile",type=str,required=True, help = "Gene locus in bed format")
-  argparser.add_argument("--sgstart",dest="sgstart",type=int, default=-1,help = "The first nucleotide sgRNA starts. 1-index")
-  argparser.add_argument("--sgstop",dest="sgstop",type=int, default=-1,help = "The last nucleotide sgRNA starts. 1-index")
+  #argparser.add_argument("--sgstart",dest="sgstart",type=int, default=-1,help = "The first nucleotide sgRNA starts. 1-index")
+  #argparser.add_argument("--sgstop",dest="sgstop",type=int, default=-1,help = "The last nucleotide sgRNA starts. 1-index")
   argparser.add_argument("--trim3",dest="trim3",type=str,help = "The trimming pattern from 3'. This pattern and the following sequence will be removed")
   return(argparser)
 

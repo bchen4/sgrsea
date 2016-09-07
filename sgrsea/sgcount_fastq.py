cd9089f690abe3d961e834c8e78da49b93f965ae
bchen4 <amybluebell@163.com>
2016-09-07 06:26:10 -0500

    multi-processing framework worked

--- sgrsea/sgcount_fastq.py
+++ sgrsea/sgcount_fastq.py
@@ -8,7 +8,7 @@ import argparse as ap
 import logging
 import pandas as pd
 import numpy as np
-from multiprocessing import Process, JoinableQueue
+from multiprocessing import Process, Queue
 logging.basicConfig(level=10)
 
 
@@ -19,28 +19,51 @@ def prepare_argparser():
   group = argparser.add_mutually_exclusive_group()
   group.add_argument("-i","--input",dest = "infile",type=str, help="input fastq")
   group.add_argument("-d","--design",dest="designfile",type=str,help = "design file")
-  argparser.add_argument("-o","--output",dest = "outfile",type=str,required=True, help="output")
-  argparser.add_argument("-l","--library",dest="libfile",type=str,required=True, help = "Gene locus in bed format")
-  argparser.add_argument("--sgstart",dest="sgstart",type=int, default=-1,help = "The first nucleotide sgRNA starts. 1-index")
-  argparser.add_argument("--sgstop",dest="sgstop",type=int, default=-1,help = "The last nucleotide sgRNA starts. 1-index")
-  argparser.add_argument("--trim3",dest="trim3",type=str,help = "The trimming pattern from 3'. This pattern and the following sequence will be removed")
+  #argparser.add_argument("-o","--output",dest = "outfile",type=str,required=True, help="output")
+  #argparser.add_argument("-l","--library",dest="libfile",type=str,required=True, help = "Gene locus in bed format")
+  #argparser.add_argument("--sgstart",dest="sgstart",type=int, default=-1,help = "The first nucleotide sgRNA starts. 1-index")
+  #argparser.add_argument("--sgstop",dest="sgstop",type=int, default=-1,help = "The last nucleotide sgRNA starts. 1-index")
+  #argparser.add_argument("--trim3",dest="trim3",type=str,help = "The trimming pattern from 3'. This pattern and the following sequence will be removed")
   return(argparser)
 
 def runsgcount(args):
-  if args.infile or args.designfile:
+  if args.infile and args.designfile:
     logging.error("You must provide infile OR design file. Quit.")
     sys.exit(1)
   if args.designfile:#parse design file
     #If design file is provided, sgRNA start, stop, trim3 will be ignored
     result = multicount(args)
   elif args.infile:
+    infile = open(args.infile,"r")
+    lib = open(args.libfile,"r")
+    outfile = open(args.outfile+".count.txt","w")
+    outsummary = open(args.outfile+".count.summary","w")
     result = sgcount(args)
-    #infile = open(args.infile,"r")
-  #lib = open(args.libfile,"r")
-  #outfile = open(args.outfile+".count.txt","w")
-  #outsummary = open(args.outfile+".count.summary","w")
 
-def sgcount():
+def testfunc(queue,pname,fname):
+  print "Process name",pname,"file name", fname
+  queue.put(pname)
+
+def multicount(args):
+  infile = open(args.designfile,"r")
+  filebuf = infile.readlines()
+  work_num = len(filebuf)
+  work_queue = Queue()
+  workers = []
+  for index in range(work_num):
+    p = Process(target=testfunc, args=(work_queue,index, filebuf[index]))
+    workers.append(p)
+    p.start()
+
+  for process in workers:
+    process.join()
+
+  result = []
+  for i in range(work_num):
+    result.append(work_queue.get())
+  print result
+
+def sgcount(fqfile, lib):
 
   libdic = {}
   dupseq = []
@@ -56,7 +79,7 @@ def sgcount():
     libdic.pop(k,None)
   mapped = 0
   total_count = 0
-  for record in infile:
+  for record in fqfile:
     total_count +=1
     #if i % 1000000 ==0:
   #    logging.info("Processed "+str(i% 1000000)+"M reads...")

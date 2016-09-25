9e72b575fbf213187377b2ca5b85c4fcf8f9f045
bchen4 <amybluebell@163.com>
2016-09-24 21:39:12 -0500

    change file names

---
+++ sgrsea/sgcount.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python
+# programmer : beibei chen
+# usage: count sgRNA from fastq file
+# last modification: 9/13/2016
+
+import sys
+from Bio import SeqIO
+import argparse as ap
+import logging
+import pandas as pd
+import numpy as np
+from multiprocessing import Process, Queue
+logging.basicConfig(level=10)
+
+
+def prepare_argparser():
+  description = "Get sgRNA counts from fastq file. For multiple files, use design file -d."
+  epilog = "For command line options of each command, type %(prog)% COMMAND -h"
+  argparser = ap.ArgumentParser(description=description, epilog = epilog)
+  group = argparser.add_mutually_exclusive_group()
+  group.add_argument("-i","--input",dest = "infile",type=str, help="input fastq")
+  group.add_argument("-d","--design",dest="designfile",type=str,help = "design file")
+  argparser.add_argument("-o","--output",dest = "outfile",type=str,required=True, help="output")
+  argparser.add_argument("-l","--library",dest="libfile",type=str, help = "Gene locus in bed format")
+  argparser.add_argument("--sgstart",dest="sgstart",type=int, default=-1,help = "The first nucleotide sgRNA starts. 1-index")
+  argparser.add_argument("--sgstop",dest="sgstop",type=int, default=-1,help = "The last nucleotide sgRNA starts. 1-index")
+  argparser.add_argument("--trim3",dest="trim3",type=str,help = "The trimming pattern from 3'. This pattern and the following sequence will be removed")
+  return(argparser)
+
+def runsgcount(args):
+  if args.infile and args.designfile:
+    logging.error("You must provide infile OR design file. Quit.")
+    sys.exit(1)
+  if args.designfile:#parse design file
+    #If design file is provided, sgRNA start, stop, trim3 will be ignored
+    dfile = pd.read_table(args.designfile)
+    libinfo = dfile.loc[:,['lib','sublib']].drop_duplicates()
+    lib = makelib(libinfo['lib'].tolist(),libinfo['sublib'].tolist())
+    lib_df = pd.DataFrame()
+    (result, total_fq_count) = multicount(dfile)
+    (result_df, summary_df) = generatefinaltable(result, total_fq_count, lib, dfile)
+  elif args.infile:
+    #infile = open(args.infile,"r")
+    lib = makelib([args.libfile],['sublib'])
+    result = {}
+    ##logging.debug("There are "+str(lib.shape[0])+" sgRNAs in the library")
+    total_fq_count = {}
+    (result_addr,total_count,sublib) = sgcount(args.infile, args.sgstart, args.sgstop, args.trim3,"count","sublib")
+    result[sublib] = pd.read_table(result_addr+".tmpcount")
+    total_fq_count[result_addr] = total_count
+  #Get total reads count df
+    (result_df, summary_df) = generatefinaltable(result, total_fq_count, lib, None)
+  result_df.to_csv(args.outfile+".count.txt",sep="\t",index=False)
+  summary_df.to_csv(args.outfile+".summary.txt",sep="\t",index=False)
+
+def generatefinaltable(resultdic, totaldic, lib, dfile):
+  '''
+    merge df based on sublib  
+  ''' 
+  #Generate count table
+  count_df = pd.DataFrame()
+  for sublibname, c_df in resultdic.items():
+    sublib = lib[sublibname]
+    df = sublib.merge(c_df,on='Sequence',how='left')
+    df = df.fillna(0)
+    count_df = count_df.append(df)
+  #Generate summary table
+  count_columns = count_df.columns.tolist()
+  count_columns.insert(0,count_columns.pop(count_columns.index('sgRNA')))
+  count_columns.insert(1,count_columns.pop(count_columns.index('Gene')))
+  count_columns.insert(2,count_columns.pop(count_columns.index('Sequence')))
+  count_columns.insert(3,count_columns.pop(count_columns.index('sublib')))
+  count_df = count_df.loc[:,count_columns]
+  mapped_total = count_df.iloc[:,3:].groupby("sublib").sum().reset_index()
+  mapped_total_df = pd.melt(mapped_total, id_vars=['sublib'],var_name=['label'],value_name='mapped_reads')
+  totalread_df = pd.DataFrame(totaldic.items(),columns=["filepath","total_reads"])
+  if dfile!=None:
+    summary_df = dfile.merge(totalread_df,on="filepath")
+    summary_df = summary_df.merge(mapped_total_df,on=['sublib','label'])
+  else:#single file
+    summary_df = totalread_df
+    summary_df = summary_df.join(mapped_total_df)
+  summary_df['mapping_ratio'] = summary_df['mapped_reads']/summary_df['total_reads']
+  summary_df = summary_df.loc[:,['filepath','label','sublib','total_reads','mapped_reads','mapping_ratio']]
+  return (count_df, summary_df)
+
+def callsgcount(queue,fname,sgstart,sgstop,trim3,label,sublib):
+  queue.put(sgcount(fname,sgstart,sgstop,trim3,label,sublib))
+
+def multicount(dfile):
+  '''
+  Parse design file, and call sgcount for each pair of files. 
+  Combine all results to a Pandas dataframe and return,group by sublib
+  '''
+  work_num = dfile.shape[0]
+  work_queue = Queue()
+  workers = []
+
+  for index in range(work_num):
+    record = dfile.iloc[index,:]
+    p = Process(target=callsgcount, args=(work_queue,record['filepath'],
+      record['sgstart'],record['sgstop'],record['trim3'],record['label'],record['sublib']))
+    workers.append(p)
+    p.start()
+  
+  logging.info("Start join process")
+  for process in workers:
+    process.join()
+  
+  #get results
+  logging.info("Retrive results")
+  total_fq_count = {}
+  groupfile = {}
+  for i in range(work_num):
+    (fqfilename,total_count,fsublib) = work_queue.get()#output should be a Pandas df
+    total_fq_count[fqfilename] = total_count
+    if not groupfile.has_key(fsublib):
+      groupfile[fsublib] = []
+    groupfile[fsublib].append(fqfilename)
+  result_dic ={}
+  for sublibname, files in groupfile.items():
+    result = pd.read_table(files[0]+".tmpcount")
+    try:
+      os.remove(files[0]+".tmpcount")
+    except:
+      logging.warning("Cannot delete "+files[0]+".tmpcount")
+    for fn in files[1:]:
+      result = result.merge(pd.read_table(fn+".tmpcount"),on="Sequence",how="outer")
+      try:
+        os.remove(fn+".tmpcount")
+      except:
+        logging.warning("Cannot delete "+fqfilename+".tmpcount")
+    result_dic[sublibname] = result
+  return (result_dic, total_fq_count)
+
+
+def makelib(libs, sublib):
+  '''
+  #Pandas dataframe columns are: ['sgRNA','Gene','Sequence','sublib']
+  return a dictionary with label as the key and df as value
+  '''
+  lib_dic = {}
+  for i in range(len(libs)):
+    df = pd.read_table(libs[i])
+    df['sublib'] = [sublib[i]]*df.shape[0]
+    seq_count = df['Sequence'].value_counts()
+    seq_count_df = pd.DataFrame({'Sequence':seq_count.index, 'Count':seq_count.values})
+    df_uniq = df[df['Sequence'].isin(seq_count_df[seq_count_df['Count']==1]['Sequence'])]
+    lib_dic[sublib[i]] = df_uniq
+  return lib_dic
+   
+def trimseq(seq,start,stop,trim3=None):
+  if len(seq)> start > 0:
+    new_start = start - 1
+  if 0 < stop <= len(seq):
+    new_stop  = stop
+  if trim3:#find the pattern from tail and trim off the rest of seq
+    trim_index = seq.rfind(trim3)
+    if trim_index >0:
+      new_stop = min(new_stop, trim_index+1)
+  trim_seq = seq[new_start:new_stop]
+  return trim_seq
+
+
+def sgcount(fqfile,sgstart, sgstop, trim3, label="count",sublib="sublib"):
+  '''
+  Count sequence frequency in a fastq file.
+  Write result in a temp file due to Python multiprocessing hang with huge result. (It can only handle int and string, not a instance of a class)
+  '''
+  total_count = 0
+  seqdic = {}
+  for record in SeqIO.parse(fqfile,"fastq"):
+    total_count += 1
+    if total_count % 500000 ==0:
+      logging.info("Processed "+fqfile+" "+str(total_count)+" reads...")
+      #break
+    sequence =  trimseq(str(record.seq),sgstart, sgstop, trim3)
+    if not seqdic.has_key(sequence):
+      seqdic[sequence] = 0
+    seqdic[sequence]+=1
+  seq_count = pd.DataFrame(seqdic.items(),columns=['Sequence',label])
+  logging.info(seq_count.shape)
+  seq_count.to_csv(fqfile+".tmpcount",sep="\t",index=False)
+  return (fqfile,total_count,sublib)
+
+def run():
+  argparser = prepare_argparser()
+  args = argparser.parse_args()
+  runsgcount(args)
+
+if __name__=="__main__":
+  run()
---
+++ sgrsea/stattest.py
@@ -0,0 +1,214 @@
+#!/usr/bin/python
+# programmer : bbc
+# usage:
+
+import sys
+import random
+import logging
+import copy
+import math
+import numpy as np
+import pandas as pd
+import argparse as ap
+#from statsmodels.stats.multitest import multipletests
+
+logging.basicConfig(format='%(levelname)s:%(message)s',level=logging.DEBUG)
+
+def prepare_argparser():
+  description = "sgRSEA main stat function"
+  epilog = "For command line options of each command, type %(prog)% COMMAND -h"
+  argparser = ap.ArgumentParser(description=description, epilog = epilog)
+  argparser.add_argument("-i","--input",dest = "infile",type=str,required=True, help = "sgRSEA input file, 4 columns")
+  argparser.add_argument("-o","--output",dest = "outfile",type=str,required=True, help = "output file name")
+  argparser.add_argument("-m","--multiplier",dest = "multiplier",type=int, default = 30,required=True, help = "Multiplier to generate background")
+  argparser.add_argument("--bgtag",dest = "bgtag",type=str, default = "",help = "Sting to identify control sgRNAs")
+  argparser.add_argument("--bg-row-start",dest = "bgrowstart",type=int,default = -1, help = "Row count of the start of control sgRNA block")
+  argparser.add_argument("--bg-row-stop",dest = "bgrowstop",type=int, default=-1, help = "Row count of the stop of control sgRNA block")
+  return(argparser)
+
+def getBackground(infile,nontag="",tagStart=0,tagStop=0):
+  '''Get background data frame from either string tag or row range.
+  '''
+  if len(nontag)>0:
+    bgFile = infile[infile.iloc[:,0].str.contains(nontag)]
+    dataFile = infile[infile.iloc[:,0].str.contains(nontag)==False]
+  elif tagStart<tagStop and tagStart<infile.shape[0]: #use the row range to get file
+    tagStart -= 1
+    tagStop = min(infile.shape[0],tagStop)
+    bgFile = infile.iloc[tagStart:tagStop,:]
+    dataFile = infile.iloc[0:tagStart-1,:].append(infile.iloc[tagStop:,:])
+  else:#Non background
+    logging.info("No designed NonTargeting sgRNAs, use input data as backgound.")
+    bgFile = copy.copy(infile)
+    dataFile = infile
+
+  logging.debug("bgFile: "+str(bgFile.shape[0]))
+  logging.debug("dataFile: "+str(dataFile.shape[0]))
+  if bgFile.shape[0]==0: #backgound file is empty
+    logging.info("There is no NonTargeting sgRNA found. Use input data as background.")
+    bgFile = copy.copy(infile)
+    dataFile = infile
+  return (dataFile,bgFile)
+
+def addZstat(data_df, pNull):
+  data_df['pmme'] = data_df['treat']/(data_df['treat']+data_df['ctrl'])
+  data_df['zstat'] = data_df._get_numeric_data().apply(lambda x: zStat(x,pNull),axis=1)
+  return data_df
+
+def dataFilter(dataFile,sg_min = 1):
+  '''Get table of sgRNA number per gene distribution. Filter out the genes with sgRNA number less than sg_min'''
+  filter_data = dataFile.groupby('Gene').filter(lambda x : len(x)>=sg_min)
+  logging.debug(type(filter_data))
+  logging.debug(filter_data.columns.values)
+  #filter_data['zstat'] = filter_data.loc[:,['treat_1','ctrl_1']].apply(lambda row: zStat(row.to_frame(['treat_1','ctrl_1']),0.4))
+  gene_label = filter_data.loc[:,'Gene']
+  return (filter_data,gene_label)
+
+
+def pMME(treat, ctrl):
+  '''1st numeric col is treatment and 2nd numeric col is control'''
+  t_total = sum(treat)
+  c_total = sum(ctrl)
+  try:
+    pmme = t_total * 1.0 / (t_total+c_total)
+  except ZeroDivisionError:
+    #logging.warning("ZeroDivisionError in pMME calculation, which indicates no reads count for treatment. Return 0 instead. Continuing")
+    pmme = -1
+  if not (0.0<=pmme<=1.0):
+    logging.error("pMME range error. Exit")
+    sys.exit(1)
+  return pmme
+
+def zStat(sg_row,pNull):
+  #logging(sg_row)
+  if pNull == 0 or pNull ==1:
+    return None
+  else:
+    zscore = (sg_row.pmme-pNull) * math.sqrt(sg_row.treat+sg_row.ctrl) / math.sqrt(pNull*(1-pNull)) 
+    return zscore
+
+def maxMean(gene_zstat):
+  scores = gene_zstat
+  sc_po = sum(scores[scores>=0])*1.0
+  sc_ne = sum(scores[scores<0])*1.0
+  if abs(sc_po) >= abs(sc_ne):
+    return sc_po/len(scores)
+  else:
+    return sc_ne/len(scores)
+
+def getMatrixMaxmean(filtered_zdf):
+  '''
+     This function returns a dataframe with 'Gene', 'maxmean', 'sgCount'    
+  '''
+  maxmean_df = filtered_zdf.groupby("Gene").agg({'zstat':maxMean,'sgRNA':'count'}).reset_index()
+  maxmean_df.columns = ["Gene","maxmean","sgCount"]
+  logging.debug(maxmean_df.head(10))
+  return maxmean_df
+
+def sampleNull(genelist,bgFile):
+  sample_size = len(genelist)
+  samples = bgFile.iloc[np.random.randint(0,len(bgFile),size=sample_size)]
+  #make samples into gene_matrix structure list
+  samples['gene'] = genelist.values
+  logging.debug(samples.head(10))
+  group_sample = samples.groupby('gene')
+  return group_sample
+
+
+def maxmeanSampleNull(genelist,bgFile,multiplier=10):
+  '''Call sampleNull() multiplier times. Only keep maxmean dataframe to save memory.'''
+  null_maxmean = pd.DataFrame()
+  np.random.seed(8512)
+  for i in range(multiplier):
+    null_group = sampleNull(genelist,bgFile)
+    newdf = getMatrixMaxmean(null_group,pNull)
+    null_maxmean = null_maxmean.append(newdf)
+  logging.debug("Null_maxmean_df")
+  logging.debug(null_maxmean[null_maxmean['sgcount'].isin([8,9])])
+  return null_maxmean
+
+def standardizeFactor(null_maxmean):
+  '''Input: null_maxmean_df, Returns a df with 'sgcount', 'mean','std' '''
+  group_null_maxmean = null_maxmean.groupby('sgcount')
+  factor_df = pd.DataFrame({'mean':group_null_maxmean.maxmean.agg(np.mean),'std':group_null_maxmean.maxmean.agg(np.std)}).reset_index()
+  return factor_df
+  
+def standardize(datarow,factors):
+  '''Datarow is a pd.Series, index is ['gene','maxmean','sgcount']'''
+  factor = factors[factors['sgcount']==datarow.sgcount]
+  if factor.shape[0]==1:#Should be only one record
+    f_mean = factor.iloc[0]['mean']
+    f_std = factor.iloc[0]['std']
+  elif factor.shape[0]==0:
+    logging.warning("There is no record for genes with "+str(datarow.sgcount+" sgRNAs. "))
+  else:#more than 1 record
+    logging.warning("There are more than 1 record for sgRNA count "+str(datarow.sgcount+" group. Check data."))
+  smm = (datarow.maxmean-f_mean)*1.0/f_std
+  return smm
+
+def standardizeDF(maxmean_df,sFactors):
+  maxmean_df['sMaxmean'] = maxmean_df.apply(lambda x: standardize(x,sFactors),axis=1)
+  return maxmean_df
+  
+def pvalue(tn,smm_null):
+  '''smm_null is a dataframe'''
+  pos_p = (smm_null[smm_null['sMaxmean']>tn].shape[0]+1.0)/(1.0+smm_null.shape[0])
+  return pos_p
+
+def getPQ(data_maxmean_std,null_maxmean_std):
+  data_maxmean_std['pos_p'] = data_maxmean_std.apply(lambda x: pvalue(x['sMaxmean'],null_maxmean_std),axis=1)
+  data_maxmean_std['neg_p'] = 1.0 -data_maxmean_std['pos_p'] 
+  data_maxmean_std['pos_q'] = multipletests(data_maxmean_std.loc[:,'pos_p'],method='fdr_bh')[1]
+  data_maxmean_std['neg_q'] = multipletests(data_maxmean_std.loc[:,'neg_p'],method='fdr_bh')[1]
+  data_maxmean_std['pos_rank'] = data_maxmean_std['pos_p'].rank()
+  data_maxmean_std['neg_rank'] = data_maxmean_std['neg_p'].rank()
+  return data_maxmean_std
+
+def runStatinfer(infile,outfile,nontag,tagStart,tagStop,multiplier):
+  if (len(nontag)!=0) or (tagStart>0 and tagStop>0):
+    (dataFile,bgFile) = getBackground(infile,nontag,tagStart,tagStop)
+    p0 = pMME(bgFile)
+  else:#Use dataset as background
+    dataFile = pd.read_table(infile)
+    p0 = pMME(dataFile.iloc[:,2],dataFile.iloc[:,3])
+  if p0 ==0 or p0 ==1:
+    logging.error("pMME for background equals to 0/1, indicating no counts for treatment or control. Please check your data. Exit.")
+    sys.exit(1)
+  logging.debug(p0) 
+  dataFile.columns = ['sgRNA','Gene','treat','ctrl']
+  dataFile = addZstat(dataFile,p0)
+  #logging.debug(dataFile.head(10))
+  dataFile.to_csv("test_real_zscore.txt",sep="\t",header=True,index=False)
+  (filtered_data,genelist) = dataFilter(dataFile,1)
+  #dataStat = treat_group.size()
+  #logging.info("Calculating treatment maxmean...")
+  data_maxmean_df = getMatrixMaxmean(filtered_data)
+  data_maxmean_df.to_csv("test_real_maxmean.txt",sep="\t",header=True,index=False)
+  #logging.info("Sampling null distribution...")
+#BC#  if isinstance(bgFile, pd.DataFrame): 
+#BC#    bgFile = addZstat(bgFile, p0)
+#BC#  else:
+#BC#    bgFile = filtered_data
+#BC#  null_maxmean_df = maxmeanSampleNull(genelist,bgFile,multiplier)
+#BC#  #logging.debug("Get standardize factors")
+#BC#  factor_df = standardizeFactor(null_maxmean_df)
+#BC#  logging.debug(factor_df)
+#BC#  #logging.info("Standardization...")
+#BC#  data_sdf = standardizeDF(data_maxmean_df,factor_df)
+#BC#  null_sdf = standardizedDF(null_maxmean_df,factor_df)
+#BC#  #sDf.to_csv("test_real_standardize.txt",sep="\t",header=True,index=False)
+#BC#  fdf = getPQ(data_sdf,null_sdf)
+#BC#  fdf.to_csv(outfile,sep="\t",index=False)
+
+def main():
+  argparser = prepare_argparser()
+  args = argparser.parse_args()
+  #infile = pd.read_table(args.infile)
+  #print pMME(infile)
+  #df = getMatrixMaxmean(infile)
+  #df.to_csv(args.outfile,sep="\t",index=False)
+  runStatinfer(args.infile,args.outfile,args.bgtag, args.bgrowstart, args.bgrowstop, args.multiplier)
+  
+if __name__ == '__main__':
+	main()
+	

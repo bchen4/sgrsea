5c080fdf45720794f783ef4c7b994a40bd6385bf
bchen4 <beibeichen1103@gmail.com>
2017-08-28 13:57:21 -0500

    remove comments

--- sgrsea/runsgrsea.py
+++ sgrsea/runsgrsea.py
@@ -9,8 +9,8 @@ import pandas as pd
 import argparse as ap
 import sgcount
 import normalization
-#import stattest
-#import reformatCountTable
+import stattest
+import reformatCountTable
 import rankgene
 from multiprocessing import Process, Queue
 logging.basicConfig(level=10)
@@ -39,9 +39,6 @@ def prepare_argparser():
   argparser.add_argument("--collapse-replicates",dest="collapsemethod",type=str, help = "Way to collapse replicates", default="None", choices=['auto','stack','mean','None'])
   argparser.add_argument("--no-count", dest="nocount", default=False, action='store_true', help="Skip counting step. Uses output contents as input")
 
-  #argparser.add_argument("--bgtag",dest = "bgtag",type=str, default = "",help = "Sting to identify control sgRNAs")
-  #argparser.add_argument("--bg-row-start",dest = "bgrowstart",type=int,default = -1, help = "Row count of the start of control sgRNA block")
-  #argparser.add_argument("--bg-row-stop",dest = "bgrowstop",type=int, default=-1, help = "Row count of the stop of control sgRNA block")
   return argparser
 
 def run(args):
@@ -60,25 +57,25 @@ def run(args):
       logging.error("There is no normalized count file. Exit.")
       sys.exit(1)
     else:# Proceed
-      rankgene.generank(args.outfile+".norm.txt", args.designfile, args.outfile, args.treat, args.ctrl, args.collapsemethod, args.multiplier, args.randomSeed)
-#BC#      if args.collapsemethod != "None":
-#BC#        files = reformatCountTable.runReformat(args.outfile+".norm.txt",args.designfile, args.outfile,args.treat, args.ctrl, args.collapsemethod)
-#BC#        if len(files)==0:
-#BC#          logging.error("There is no input files for sgRSEA to run. Exit.")
-#BC#          sys.exit(1)
-#BC#        else:
-#BC#          work_num = len(files)
-#BC#          work_queue = Queue()
-#BC#          workers = []
-#BC#          for fn in files:
-#BC#            logging.info("Running test on "+fn)
-#BC#            p = Process(target = callstat, args=(work_queue,fn,fn+".sgRSEA.xls",args.multiplier, args.randomSeed))
-#BC#            workers.append(p)
-#BC#            p.start()
-#BC#          for process in workers:
-#BC#            process.join()
-#BC#      else:#Use GM ranking for replicates
-#BC#        rankgene.run(args.outfile+".norm.txt", args.designfile, args.outfile, args.treat, args.ctrl, args.multiplier, args.randomSeed)
+      #rankgene.generank(args.outfile+".norm.txt", args.designfile, args.outfile, args.treat, args.ctrl, args.collapsemethod, args.multiplier, args.randomSeed)
+      if args.collapsemethod != "None":
+        files = reformatCountTable.runReformat(args.outfile+".norm.txt",args.designfile, args.outfile,args.treat, args.ctrl, args.collapsemethod)
+        if len(files)==0:
+          logging.error("There is no input files for sgRSEA to run. Exit.")
+          sys.exit(1)
+        else:
+          work_num = len(files)
+          work_queue = Queue()
+          workers = []
+          for fn in files:
+            logging.info("Running test on "+fn)
+            p = Process(target = callstat, args=(work_queue,fn,fn+".sgRSEA.xls",args.multiplier, args.randomSeed))
+            workers.append(p)
+            p.start()
+          for process in workers:
+            process.join()
+      else:#Use GM ranking for replicates
+        rankgene.run(args.outfile+".geoRank.txt", args.designfile, args.outfile, args.treat, args.ctrl, args.multiplier, args.randomSeed)
 
 def callstat(queue, fname, outname, multiplier, randomseed):
   queue.put(stattest.runStatinfer(fname,outname,multiplier,randomseed))
